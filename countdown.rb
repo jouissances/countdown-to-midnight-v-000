67e68299deaedf66ef0971242f76da747f90da2a
Janice Darikho <janicedarikho@gmail.com>
2018-05-19 05:28:41 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -11,6 +11,9 @@ end
 
 puts "HAPPY NEW YEAR!"
 
-def countdown_with_sleep(number, seconds)
-  
+def countdown_with_sleep(number)
+  while number > 0
+  puts "#{number} SECOND(S)!"
+  number -= 1
+  sleep(1)
 end
